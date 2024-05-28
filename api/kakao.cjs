8beb34519ccfe749e1ae1825e37b3088f4b519e6
thesandfox <sandfox9411@gmail.com>
2024-05-28 10:02:26 +0900

    fix domain

--- api/kakao.cjs
+++ api/kakao.cjs
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const KakaoLoginAPI = require('../KakaoLoginAPI.cjs');
 const config = require('../config.cjs');
+const domain = config.DEBUG
+?'http://localhost:5173'
+:'https://thesandfox.github.io'
 
 //로그인 시 토큰요청
 router.post('/requestToken',(req,res)=>{
@@ -10,12 +13,12 @@ router.post('/requestToken',(req,res)=>{
 		//로그인성공;
 		console.log('성공맨'+response.data['access_token']);
 		res.cookie('accessToken',response.data['access_token'],{
-			domain:config.REDIRECT_URI,
+			domain,
 			sameSite:'none',
 			secure:true
 		});
 		res.cookie('refreshToken',response.data['refresh_token'],{
-			domain:config.REDIRECT_URI,
+			domain,
 			sameSite:'none',
 			secure:true
 		});
